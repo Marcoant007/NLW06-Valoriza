f240ef323b1a9b1dd75fa299dd592ce610f8324d
Marcoant007 <marcoantnovo@gmail.com>
2021-06-26 22:17:23 -0300

    Adicionando docker e banco de dados postgres

--- src/services/ListUserReceiveComplimentsService.ts
+++ src/services/ListUserReceiveComplimentsService.ts
@@ -1,9 +1,6 @@
 import { getCustomRepository } from "typeorm"
 import ComplimentsRepositories from "../repositories/ComplimentsRepositories";
-import UsersRepositories from "../repositories/UsersRepository"
-
 class ListUserReceiveComplimentsService {
-
     async execute(user_id){
         const complimentsRepository = getCustomRepository(ComplimentsRepositories);
         const compliments = await complimentsRepository.find({
